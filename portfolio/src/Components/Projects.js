cc4a918c17756af1ae23dd40ff11902354b0f7b1
NoaFilosof <noafilosof@hotmail.co.uk>
2020-09-30 10:23:05 +0100

    changes made to projects section

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -5,24 +5,40 @@ const Project = () => {
     <div id="project" className="section">
       <div className="tile is-ancestor">
         <div className="tile is-6">
-          <h2 className="title">Project 1: RuPaul's Space invaders</h2>
-          <p>About the project</p>
-          <p>placeholder for links</p>
+          <div className="proj">
+            <h2 className="title">Project 1: RuPaul's Space invaders</h2>
+          </div>
+          <div className="proj">
+            <p>About the project</p>
+            <p>placeholder for links</p>
+          </div>
         </div>
         <div className="tile is-6">
-          <h2 className="title">Project 2: Dwitanic</h2>
-          <p>About the project</p>
-          <p>placeholder for links</p>
+          <div className="proj">
+            <h2 className="title">Project 2: Dwitanic</h2>
+          </div>
+          <div className="proj">
+            <p>About the project</p>
+            <p>placeholder for links</p>
+          </div>
         </div>
         <div className="tile is-6">
-          <h2 className="title">Project 3: GigMe</h2>
-          <p>About the project</p>
-          <p>placeholder for links</p>
+          <div className="proj">
+            <h2 className="title">Project 3: GigMe</h2>
+          </div>
+          <div className="proj">
+            <p>About the project</p>
+            <p>placeholder for links</p>
+          </div>
         </div>
         <div className="tile is-6">
-          <h2 className="title">Project 4: Artly</h2>
-          <p>About the project</p>
-          <p>placeholder for links</p>
+          <div className="proj">
+            <h2 className="title">Project 4: Artly</h2>
+          </div>
+          <div className="proj">
+            <p>About the project</p>
+            <p>placeholder for links</p>
+          </div>
         </div>
       </div>
     </div>
