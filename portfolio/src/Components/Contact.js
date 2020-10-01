dbd2699622c76dfadb043bde6e5c8457a60979c5
NoaFilosof <noafilosof@hotmail.co.uk>
2020-10-01 15:12:35 +0100

    spacing for projects section complete

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Contact = () => {
   return (
     <div id="contact">
-      <h1>Contact Page</h1>
+      <div className="section">
+        <div className="container">
+          <h1>Contact Page</h1>
+          <div className="tabs is-centered is-boxed is-medium">
+            <ul>
+              <li className="is-active"><span>Email</span></li>
+              <li><span>GitHub</span></li>
+              <li><span>LinkedIn</span></li>
+              {/* <li><span>Email</span></li> */}
+            </ul>
+
+          </div>
+        </div>
+      </div>
+
       <p>noafilosof@hotmail.co.uk</p>
       <p>https://github.com/NoaFilosof08</p>
       <p>https://www.linkedin.com/in/noa-filosof/</p>
--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -3,42 +3,48 @@ import React from 'react'
 const Project = () => {
   return (
     <div id="project" className="section">
-      <div className="tile is-ancestor">
-        <div className="tile is-6 proj1">
-          {/* <div className="proj"> */}
-            <h2 className="title">Project 1: RuPaul's Space invaders</h2>
-          {/* </div> */}
-          {/* <div className="proj"> */}
-            <p>About the project</p>
-            <p>placeholder for links</p>
-          {/* </div> */}
-        </div>
-        <div className="tile is-6 proj1">
-          {/* <div className="proj"> */}
-            <h2 className="title">Project 2: Dwitanic</h2>
-          {/* </div>
-          <div className="proj"> */}
-            <p>About the project</p>
-            <p>placeholder for links</p>
-          {/* </div> */}
-        </div>
-        <div className="tile is-6 proj1">
-          {/* <div className="proj"> */}
-            <h2 className="title">Project 3: GigMe</h2>
-          {/* </div>
-          <div className="proj"> */}
-            <p>About the project</p>
-            <p>placeholder for links</p>
-          {/* </div> */}
-        </div>
-        <div className="tile is-6 proj1">
-          {/* <div className="proj"> */}
-            <h2 className="title">Project 4: Artly</h2>
-          {/* </div>
-          <div className="proj"> */}
-            <p>About the project</p>
-            <p>placeholder for links</p>
-          {/* </div> */}
+      <div className="cont">
+        <div className="tile is-ancestor">
+          <div className="tile is-6 proj1 is-parent">
+            <div className="proj tile is-child box">
+              <h2 className="title">Project 1: RuPaul's Space invaders</h2>
+              <p>About the project</p>
+              <p>placeholder for links</p>
+            </div>
+            {/* <div className="proj"> */}
+
+            {/* </div> */}
+          </div>
+          <div className="tile is-6 proj1 is-parent">
+            <div className="proj tile is-child box">
+              <h2 className="title">Project 2: Dwitanic</h2>
+              <p>About the project</p>
+              <p>placeholder for links</p>
+            </div>
+            {/* <div className="proj"> */}
+
+            {/* </div> */}
+          </div>
+          <div className="tile is-6 proj1 is-parent">
+            <div className="proj tile is-child box">
+              <h2 className="title">Project 3: GigMe</h2>
+              <p>About the project</p>
+              <p>placeholder for links</p>
+            </div>
+            {/* <div className="proj"> */}
+
+            {/* </div> */}
+          </div>
+          <div className="tile is-6 proj1 is-parent">
+            <div className="proj tile is-child box">
+              <h2 className="title">Project 4: Artly</h2>
+              <p>About the project</p>
+              <p>placeholder for links</p>
+            </div>
+            {/* <div className="proj">  */}
+
+            {/* </div> */}
+          </div>
         </div>
       </div>
     </div>
