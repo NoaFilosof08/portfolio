e04e73cdafd25e8b90fc7c77052fc1e51f6f4ec5
NoaFilosof <noafilosof@hotmail.co.uk>
2020-10-06 13:13:01 +0100

    styling to navbar and projects section

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -4,21 +4,21 @@ import { HashLink as Link } from 'react-router-hash-link'
 export default class Navbar extends Component {
   render() {
     return (
-      // <div ref={this.wrapperRef} className="icon">
-        <nav className="navbar is-fixed-top home">
-          <div className="container">
+      <div className="has-navbar-fixed-top">
+        <nav className="navbar is-fixed-top">
+          {/* <div className="container nav"> */}
 
-              <div className="navbar home">
+              <div className="navbar">
                 <Link to="/#about" className="navbar-item" spy={true} smooth={true} duration={1000} >about</Link>
                 <Link to="/#project" className="navbar-item" spy={true} smooth={true} duration={1000}>projects</Link>
-                <Link to="/#home" className="navbar-item" spy={true} smooth={true} duration={1000}>NF</Link>
+                <Link to="/#home" className="navbar-item bold" spy={true} smooth={true} duration={1000}>NF</Link>
                 <Link to="/#interests" className="navbar-item" spy={true} smooth={true} duration={1000}>interests</Link>
                 <Link to="/#contact" className="navbar-item" spy={true} smooth={true} duration={1000}>contact</Link>
               </div>
 
-          </div>
+          {/* </div> */}
         </nav>
-      // </div>
+      </div>
     )
   }
 }
