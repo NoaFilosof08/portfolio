4c6713e649c776881abeb602b0e66519d11e453e
NoaFilosof <noafilosof@hotmail.co.uk>
2020-10-14 12:11:33 +0100

    Updates

---
+++ precache-manifest.0e267a46673a4ca7b561e9391a50b356.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a8aae301a12779ba0f3654b691b24d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c0393ae45a1f769f383",
+    "url": "/portfolio/static/css/main.33f15748.chunk.css"
+  },
+  {
+    "revision": "022a7f8a090ea6040bc5",
+    "url": "/portfolio/static/js/2.c7136e3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c7136e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0393ae45a1f769f383",
+    "url": "/portfolio/static/js/main.cf0859f7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "36bb023c28419a62fafd7fe3b36c17cb",
+    "url": "/portfolio/static/media/Intro-page.36bb023c.png"
+  },
+  {
+    "revision": "3096f495dbc0c4cc3e3a2f3ecf08cc25",
+    "url": "/portfolio/static/media/Splash-page.3096f495.png"
+  },
+  {
+    "revision": "cf5a65b721be5eb0ee470293bbf4c941",
+    "url": "/portfolio/static/media/beach.cf5a65b7.jpg"
+  },
+  {
+    "revision": "0113fe9db36d6c448ab6df4398f2d571",
+    "url": "/portfolio/static/media/home-page.0113fe9d.png"
+  },
+  {
+    "revision": "44470300c4b4e6d277e9539a8e7b553b",
+    "url": "/portfolio/static/media/intro-page-proj2.44470300.png"
+  }
+]);
